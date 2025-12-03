cf7fd5bfc96b59c76fb1ab2e142b2637a8b3f993
JanUwU42 <coding@jan-osing.simplelogin.com>
2025-12-03 21:46:24 +0100

    indent

--- PasswordReset.vue
+++ PasswordReset.vue
@@ -1,39 +1,39 @@
 <script>
 export default {
-  data() {
-    return {
-      email: '',
-      message: ''
-    };
-  },
-  methods: {
-    handleSubmit() {
-      if (!this.email.includes('@')) {
-        this.message = 'Please enter a valid email';
-        return;
-      }
+    data() {
+        return {
+            email: "",
+            message: "",
+        };
+    },
+    methods: {
+        handleSubmit() {
+            if (!this.email.includes("@")) {
+                this.message = "Please enter a valid email";
+                return;
+            }
 
-      this.message = 'Reset link sent to ' + this.email;
-      this.email = '';
-    }
-  }
+            this.message = "Reset link sent to " + this.email;
+            this.email = "";
+        },
+    },
 };
 </script>
 
 <template>
-  <div class="reset-form">
-    <h2>Reset Password</h2>
+    <div class="reset-form">
+        <h2>Reset Password</h2>
 
-    <form @submit.prevent="handleSubmit">
-      <input
-        type="email"
-        v-model="email"
-        placeholder="Enter your email"
-      />
+        <form @submit.prevent="handleSubmit">
+            <input
+                type="email"
+                v-model="email"
+                placeholder="Enter your email"
+            />
 
-      <button type="submit">Send Reset Link</button>
-    </form>
+            <button type="submit">Send Reset Link</button>
+        </form>
 
-    <p v-if="message">{{ message }}</p>
-  </div>
+        <p v-if="message">{{ message }}</p>
+    </div>
 </template>
